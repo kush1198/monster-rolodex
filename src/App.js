ad651fad22fa0bc8aff9eee50f876c3f2dbe54db
Kushagra Bhatia <kushagra1198@gmail.com>
2020-12-30 00:29:07 +0530

    changing font

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component{
     const filteredMonsters=monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className='App'>
-        <h1> Monster Rolodex</h1>
+        <h1 className='head'> Monster Rolodex</h1>
         <SearchBox placeholder='search monster' handleChanges={e => this.setState({searchField: e.target.value})}/>
         <CardList monsters={filteredMonsters}/>
       </div>
